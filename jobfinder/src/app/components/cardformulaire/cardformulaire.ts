3eb9b4e7956ee69f0bbef16df1a95ed76409e4c1
Camille <camillebujotzek02@gmail.com>
2026-03-07 12:55:33 +0100

    implement route

--- jobfinder/src/app/components/cardformulaire/cardformulaire.ts
+++ jobfinder/src/app/components/cardformulaire/cardformulaire.ts
@@ -1,47 +1,62 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cardformulaire',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './cardformulaire.html',
   styleUrl: './cardformulaire.scss'
 })
 export class Cardformulaire {
-  nom: string = '';
-  prenom: string = '';
-  description: string = '';
-  cvFile: File | null = null;
+  private formBuilder=inject(FormBuilder);
+  private httpClient= inject(HttpClient);
+
+  protected formGroup = this.formBuilder.group({
+    jobTitle : this.formBuilder.control('', {validators : [Validators.required], nonNullable : true}),
+    jobLocation : this.formBuilder.control('', {validators : [Validators.required], nonNullable : true}),
+    applicantDescription : this.formBuilder.control('', {validators : [Validators.required], nonNullable : true}),
+  })
+
+  cvFile: File | undefined;
 
   constructor(private router: Router) {}
 
-  get formulaireValide(): boolean {
-    return (
-      this.nom.trim() !== '' &&
-      this.prenom.trim() !== '' &&
-      this.description.trim() !== '' &&
-      this.cvFile !== null
-    );
-  }
 
+  isValidForm() {
+    return this.formGroup.valid && !!this.cvFile ; 
+  }
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
 
     if (input.files && input.files.length > 0) {
       this.cvFile = input.files[0];
     } else {
-      this.cvFile = null;
+      this.cvFile = undefined;
     }
   }
 
-  validerEtChercher(): void {
-    if (!this.formulaireValide) {
-      return;
-    }
+  validateAndSearch() { 
+    if(!this.cvFile)
+      return ;
+    // corps de la requête: multipart/form-data
+    const formData = new FormData();
+    formData.append('cv', this.cvFile, this.cvFile.name);
+    formData.append('job_title', this.formGroup.controls.jobTitle.value);
+    formData.append('job_location', this.formGroup.controls.jobLocation.value);
+    formData.append('applicant_description', this.formGroup.controls.applicantDescription.value);
 
-    this.router.navigate(['/emplois']);
+    this.httpClient.post(
+      "/api-python/jobfinder/find_jobs",
+      formData,
+    ).subscribe((data) => {
+      console.log(data)
+      this.router.navigate(['/jobs'], {
+      state: { data }
+      });    
+    })
   }
 }
